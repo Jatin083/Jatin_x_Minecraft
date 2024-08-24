274924a13c7084ec6a229834ca7545c05abdf9c0
Jatin <125881431+Jatin083@users.noreply.github.com>
2024-08-24 21:17:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let object;
+let controls;
+let objToRender = 'eye';
+
+const loader = new GLTFLoader();
+loader.load(
+    `explore/minecraft_command_block/scene.gltf`,
+    function (gltf) {
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        console.log('An error occurred:', error);
+    }
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("container3D").appendChild(renderer.domElement);
+
+camera.position.z = objToRender === "dino" ? 25 : 500;
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+scene.add(ambientLight);
+
+function animate() {
+    requestAnimationFrame(animate);
+    if (object) {
+        object.rotation.y += 0.01; // Rotate the object for some animation
+    }
+    renderer.render(scene, camera); // Render the scene
+}
+
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
